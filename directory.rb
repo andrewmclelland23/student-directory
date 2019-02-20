e345e66c80350fe2bd1cabbf8cee7908e1dbf887
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-02-20 14:04:26 +0000

    Modified print method to only print students that begin with an 'A'

--- directory.rb
+++ directory.rb
@@ -18,7 +18,9 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index {|student, i| puts "#{i + 1} #{student[:name]} (#{student[:cohort]} cohort)"}
+  students.each_with_index do |student, i|
+    puts "#{i + 1} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == "A"
+  end
 end
 
 def print_footer(students)
