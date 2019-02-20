8620d5c4763dcb09c36452c8fb342761f7e7e2b2
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-02-20 10:32:15 +0000

    Included a method to interactively take in new students from the user

--- directory.rb
+++ directory.rb
@@ -25,6 +25,24 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students\nTo finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # get the first names
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of input_students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
